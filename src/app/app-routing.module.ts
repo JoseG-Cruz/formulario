db53b891a0babc12fbc5bf8d6594ec64cbd39b58
Jose Gudalupe Cortes Cruz <jose17y10@gmail.com>
2020-11-11 22:43:11 -0600

    Creacion de formularios y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { TemplateComponent } from './pages/template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'template', component: TemplateComponent},
+  { path:'reactivo', component: ReactiveComponent},
+  { path: '**', pathMatch:'full', redirectTo:'template' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
