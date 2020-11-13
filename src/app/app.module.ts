c26860e0e95ade0fde30de88714fdbb49b53c757
Jose Gudalupe Cortes Cruz <jose17y10@gmail.com>
2020-11-13 13:43:17 -0600

    Creacion de formulario rectivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,7 +19,8 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
     
   ],
   providers: [],
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor( private fb: FormBuilder ) { 
+
+    this.crearFormulario();
+
+  }
 
   ngOnInit(): void {
   }
+
+  crearFormulario(){
+
+    this.forma= this.fb.group({
+      nombre: ['Jose'],
+      apellido: ['Cortes'],
+      correo: ['jose@gmail.com'],
+      
+    });
+      
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
+  
 
 }
