e538e10f3f64f1a5e2f446b9a9665595d3d281f7
Jose Gudalupe Cortes Cruz <jose17y10@gmail.com>
2020-11-13 14:15:58 -0600

    Validaciones sicronas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -22,9 +22,9 @@ export class ReactiveComponent implements OnInit {
   crearFormulario(){
 
     this.forma= this.fb.group({
-      nombre: ['Jose'],
-      apellido: ['Cortes'],
-      correo: ['jose@gmail.com'],
+      nombre: ['', [Validators.required, Validators.minLength(5) ]],
+      apellido: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       
     });
       
