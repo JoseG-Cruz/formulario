65d5052f8473341f59c7c8fb7a742f6bd84a153e
Jose Gudalupe Cortes Cruz <jose17y10@gmail.com>
2020-11-13 13:18:49 -0600

    Uso de radio buttons

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -13,7 +13,8 @@ export class TemplateComponent implements OnInit {
     nombre: 'Jose Guadalupe',
     apellido: 'Cortes',
     correo: 'jose17y10@gmail.com',
-    pais: ''
+    pais: 'CRI',
+    genero: 'M'
   }
 
   paises: any[] = [];
@@ -31,7 +32,7 @@ export class TemplateComponent implements OnInit {
           codigo: ''
         })
 
-        console.log(paises);
+        //console.log(paises);
       });
 
   }
