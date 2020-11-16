80eaeb154ff7e3f577cfe73567105654172edd29
Jose Gudalupe Cortes Cruz <jose17y10@gmail.com>
2020-11-16 17:47:16 -0600

    validacion de contraseñas 1 y 2

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -46,6 +46,18 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
   }
 
+  get pass1NoValido(){
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched
+  }
+
+  get pass2NoValido(){
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2) ? false: true;
+
+  }
+
 
   crearFormulario(){
 
@@ -53,15 +65,17 @@ export class ReactiveComponent implements OnInit {
       nombre: ['', [Validators.required, Validators.minLength(5) ]],
       apellido: ['', [Validators.required, this.validadores.noHerrera]],
       correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      pass1: ['', Validators.required],
+      pass2: ['', Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad: ['', Validators.required],
 
       }),
-      pasatiempos: this.fb.array([
-        
-      ])
+      pasatiempos: this.fb.array([])
       
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
     });
       
   }
--- src/app/services/validadores.service.ts
+++ src/app/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +19,20 @@ export class ValidadoresService {
   return null;
 
   } 
+
+  passwordsIguales(pass1Name: string, pass2Name: string){
+    return (formGroup: FormGroup)=>{
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass2Name];
+
+      if ( pass1Control.value === pass2Control.value){
+        pass2Control.setErrors(null);
+      }else{
+        pass2Control.setErrors({noEsIgual: true});
+      }
+
+    }
+  }
 
 
 }
